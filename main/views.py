4b4b8c1855778c5c382bef35ae785b1df371eb26
Korb1t <oles.hobor@gmail.com>
2019-02-08 17:08:04 +0200

    send_email

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
 from main.models import Work
 
 
@@ -17,12 +19,30 @@ def about(request):
     return render(request, 'about.html', ctx)
 
 
+def send_email(request):
+    fname = request.POST.get('fname', '')
+    lname = request.POST.get('lname', '')
+    subject = request.POST.get('subject', '')
+    message = request.POST.get('message', '')
+    message = '{} {} /n {}'.format(fname, lname, message)
+    from_email = request.POST.get('email', '')
+    if subject and message and from_email:
+        try:
+            send_mail(subject, message, 'nightsktstudiosofficial@gmail.com', ['oles.hobor@gmail.com'])
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return HttpResponseRedirect('/contact/')
+    else:
+        return HttpResponse('Make sure all fields are entered and valid.')
+
+
 def contact(request):
     ctx = {'contact': True}
 
+    if request.method == 'POST':
+        send_email(request)
+
     return render(request, 'contact.html', ctx)
-
-
 
 
 def work(request):
