d5c0f1989868f9e83a3c1b3d2d72dfb9156e05a5
Korb1t <oles.hobor@gmail.com>
2019-02-12 14:49:40 +0200

    imgs & localization

--- recruit/settings.py
+++ recruit/settings.py
@@ -115,9 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = ''
